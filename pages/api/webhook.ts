00c5fcc5a6d58f1d56e5169e3671f65e5bba24db
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-09-11 23:52:10 +0900

    update webhook

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -1,9 +1,60 @@
 import { NextApiHandler } from "next";
 
+type Body = {
+  service: string;
+  api: "products" | "skus" | "schedule";
+  id: string | null;
+  type: "edit" | "new" | "delete";
+  contents: {
+    old: {
+      publishValue: {
+        productCode: string;
+      } | null;
+    } | null;
+    new: {
+      publishValue: {
+        productCode: string;
+      } | null;
+    } | null;
+  };
+};
+
 const handler: NextApiHandler = async (req, res) => {
   try {
-    const handle: string = req.body.handle;
-    await res.revalidate(`/${handle}`);
+    const body: Body = req.body;
+    if (body.api !== "products") return res.status(200).send({});
+    if (
+      body.contents.old?.publishValue?.productCode ===
+      body.contents.new?.publishValue?.productCode
+    ) {
+      await res.revalidate(`/${body.contents.new?.publishValue?.productCode}`);
+      console.log(
+        "revalidate",
+        `/${body.contents.new?.publishValue?.productCode}`
+      );
+    }
+    if (
+      body.contents.old?.publishValue?.productCode &&
+      body.contents.old.publishValue.productCode !==
+        body.contents.new?.publishValue?.productCode
+    ) {
+      await res.revalidate(`/${body.contents.old.publishValue.productCode}`);
+      console.log(
+        "revalidate",
+        `/${body.contents.old.publishValue.productCode}`
+      );
+    }
+    if (
+      body.contents.new?.publishValue?.productCode &&
+      body.contents.old?.publishValue?.productCode !==
+        body.contents.new.publishValue.productCode
+    ) {
+      await res.revalidate(`/${body.contents.new.publishValue.productCode}`);
+      console.log(
+        "revalidate",
+        `/${body.contents.new.publishValue.productCode}`
+      );
+    }
 
     return res.status(200).send({});
   } catch (err) {

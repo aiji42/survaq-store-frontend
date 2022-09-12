a60b5b5a0d224cf988e4aeca497643b173683f30
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-09-12 23:15:18 +0900

    update webhook

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -30,16 +30,21 @@ const handler: NextApiHandler = async (req, res) => {
     const body: Body = req.body;
     if (body.api !== "products") return res.status(200).send({});
 
+    let oldEndpoint: string = "",
+      newEndpoint: string = "";
+
     if (body.contents.old?.publishValue?.pageData?.domain) {
-      await fetch(
-        `https://${body.contents.old.publishValue.pageData.domain}/api/revalidate/${body.contents.old.publishValue.productCode}`
-      );
+      oldEndpoint = `https://${body.contents.old.publishValue.pageData.domain}/api/revalidate/${body.contents.old.publishValue.productCode}`;
     }
     if (body.contents.new?.publishValue?.pageData?.domain) {
-      await fetch(
-        `https://${body.contents.new.publishValue.pageData.domain}/api/revalidate/${body.contents.new.publishValue.productCode}`
-      );
+      newEndpoint = `https://${body.contents.new.publishValue.pageData.domain}/api/revalidate/${body.contents.new.publishValue.productCode}`;
     }
+
+    await Promise.all(
+      [...new Set([oldEndpoint, newEndpoint])]
+        .filter(Boolean)
+        .map((endpoint) => fetch(endpoint))
+    );
 
     return res.status(200).send({});
   } catch (err) {

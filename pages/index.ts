d7a960eb4d900f78815d3a6d5992284f5bdccf02
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-09-12 22:44:02 +0900

    update top page

---
+++ pages/index.ts
@@ -0,0 +1,33 @@
+import { GetServerSideProps } from "next";
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  if (!req.headers.host)
+    return {
+      notFound: true,
+    };
+
+  const res = await fetch(
+    "https://survaq-api-production.aiji422990.workers.dev/products/page-data/by-domain/" +
+      "survaq-gymshocks.com"
+  );
+  if (!res.ok)
+    return {
+      notFound: true,
+    };
+
+  const data = await res.json();
+
+  if (data.productCode)
+    return {
+      redirect: {
+        destination: `/${data.productCode}`,
+        statusCode: 302,
+      },
+    };
+
+  return {
+    notFound: true,
+  };
+};
+
+export default () => null;

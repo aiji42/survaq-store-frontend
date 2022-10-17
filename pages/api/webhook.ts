5939f5c6ecefa3ed4448ba48a430b650146bbf56
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-10-17 23:19:21 +0900

    update webhook

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -11,6 +11,11 @@ type Body = {
         productCode: string;
         pageData?: {
           domain?: string;
+          pathname?: string;
+        };
+        pageDataSub?: {
+          domain?: string;
+          pathname?: string;
         };
       } | null;
     } | null;
@@ -19,6 +24,11 @@ type Body = {
         productCode: string;
         pageData?: {
           domain?: string;
+          pathname?: string;
+        };
+        pageDataSub?: {
+          domain?: string;
+          pathname?: string;
         };
       } | null;
     } | null;
@@ -31,17 +41,25 @@ const handler: NextApiHandler = async (req, res) => {
     if (body.api !== "products") return res.status(200).send({});
 
     let oldEndpoint: string = "",
-      newEndpoint: string = "";
+      newEndpoint: string = "",
+      oldEndpointSub: string = "",
+      newEndpointSub: string = "";
 
     if (body.contents.old?.publishValue?.pageData?.domain) {
-      oldEndpoint = `https://${body.contents.old.publishValue.pageData.domain}/api/revalidate/${body.contents.old.publishValue.productCode}`;
+      oldEndpoint = `https://${body.contents.old.publishValue.pageData.domain}/api/revalidate/${body.contents.old.publishValue.pageData.pathname}`;
     }
     if (body.contents.new?.publishValue?.pageData?.domain) {
-      newEndpoint = `https://${body.contents.new.publishValue.pageData.domain}/api/revalidate/${body.contents.new.publishValue.productCode}`;
+      newEndpoint = `https://${body.contents.new.publishValue.pageData.domain}/api/revalidate/${body.contents.new.publishValue.pageData.pathname}`;
+    }
+    if (body.contents.old?.publishValue?.pageDataSub?.domain) {
+      oldEndpointSub = `https://${body.contents.old.publishValue.pageDataSub.domain}/api/revalidate/${body.contents.old.publishValue.pageDataSub.pathname}`;
+    }
+    if (body.contents.new?.publishValue?.pageDataSub?.domain) {
+      newEndpointSub = `https://${body.contents.new.publishValue.pageDataSub.domain}/api/revalidate/${body.contents.new.publishValue.pageDataSub.pathname}`;
     }
 
     await Promise.all(
-      [...new Set([oldEndpoint, newEndpoint])]
+      [...new Set([oldEndpoint, newEndpoint, oldEndpointSub, newEndpointSub])]
         .filter(Boolean)
         .map((endpoint) => fetch(endpoint))
     );

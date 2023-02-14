02d3998804c29e2c85502e39ff573d8e55f35ce0
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-02-14 09:46:54 +0900

    redirect on root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,2 +1,16 @@
-// TODO: redirect
-export default () => null;
+import { headers } from "next/headers";
+import { notFound, redirect } from "next/navigation";
+
+export default async () => {
+  const host = headers().get("host");
+
+  const res = await fetch(
+    `${process.env.SURVAQ_API_ORIGIN}/products/page-data/by-domain/${host}/supabase`
+  );
+  if (res.status === 404) return notFound();
+  if (res.status !== 200) throw new Error(await res.text());
+
+  const json = (await res.json()) as { pathname: string };
+
+  return redirect(json.pathname);
+};

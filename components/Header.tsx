86c0f61f6da5d7a09f25fddc968053db60df48be
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-02-15 14:29:23 +0900

    bugfix

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,7 @@
-"use client";
 import Image from "next/legacy/image";
 import { ProductPageData } from "@/libs/getProduct";
-import { useReplaceSchedule } from "@/libs/hooks/useReplaceSchedule";
 
 export const Header = (propduct: ProductPageData) => {
-  // TODO
-  useReplaceSchedule(propduct);
   return (
     <header className="item_header">
       {propduct.logo && (

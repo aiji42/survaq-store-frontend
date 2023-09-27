4d0070d623e49393381058b088b10c4ff8f81c5c
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2023-09-27 22:35:21 +0900

    カスタムヘッドからインラインスクリプトだけでなくurlスクリプトも埋め込み可能にする

--- components/ProductPageCustomHead.tsx
+++ components/ProductPageCustomHead.tsx
@@ -1,22 +1,51 @@
 import { ProductPageData } from "@/libs/getProduct";
 import Script from "next/script";
 
+type ScriptContent = {
+  type: "url" | "inline";
+  content: string;
+};
+
+const simpleHash = (str: string) => {
+  return Array.from(str)
+    .reduce((acc, char) => {
+      acc = (acc << 5) - acc + char.charCodeAt(0);
+      return acc & acc; // Convert to a 32bit integer
+    }, 0)
+    .toString(16);
+};
+
 export const ProductPageCustomHead = ({ customHead }: ProductPageData) => {
-  const scripts = Array.from(
-    customHead?.replaceAll("\n", "")?.matchAll(/<script.?>(.+?)<\/script>/g) ??
-      []
-  ).flatMap(([, s]) => (s ? s : []));
+  const scripts = [
+    ...(customHead
+      ?.replaceAll("\n", "")
+      ?.matchAll(/<script\s*(?:src="([^"]+)")?.*?>(.*?)<\/script>/g) ?? []),
+  ].reduce<ScriptContent[]>((acc, [, url, inlineScript]) => {
+    if (url) acc.push({ type: "url", content: url });
+    if (inlineScript.trim())
+      acc.push({ type: "inline", content: inlineScript.trim() });
+    return acc;
+  }, []);
 
   return (
     <>
-      {scripts.map((script, i) => (
-        <Script
-          id={script}
-          key={i}
-          dangerouslySetInnerHTML={{ __html: script }}
-          strategy="afterInteractive"
-        />
-      ))}
+      {scripts.map(({ type, content }, i) =>
+        type === "inline" ? (
+          <Script
+            id={simpleHash(content)}
+            key={i}
+            dangerouslySetInnerHTML={{ __html: content }}
+            strategy="afterInteractive"
+          />
+        ) : (
+          <Script
+            id={simpleHash(content)}
+            key={i}
+            src={content}
+            strategy="afterInteractive"
+          />
+        )
+      )}
     </>
   );
 };

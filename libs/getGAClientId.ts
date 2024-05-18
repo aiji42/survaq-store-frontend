100ad96900bdd2f42a78f8d277570cf6d50dbc3f
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2024-05-19 00:12:23 +0900

    fix mesurement id

--- libs/getGAClientId.ts
+++ libs/getGAClientId.ts
@@ -7,7 +7,7 @@ declare global {
 export const getGAClientId = (): undefined | string => {
   if (typeof window === "undefined" || !('gtag' in window)) return undefined;
   let clientId: string | undefined;
-  window.gtag?.('get', 'GA_MEASUREMENT_ID', 'client_id', (client_id: string) => {
+  window.gtag?.('get', 'G-VLGZ0MSKT6', 'client_id', (client_id: string) => {
     clientId = client_id;
   })
   return clientId;

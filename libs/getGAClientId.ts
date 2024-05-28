47ec898379d7927df4ecfb24b7323dad36ce567e
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2024-05-28 14:36:37 +0900

    measurementIdを変更

--- libs/getGAClientId.ts
+++ libs/getGAClientId.ts
@@ -8,7 +8,7 @@ export const getGAClientId = async  (): Promise<undefined | string> => {
   if (typeof window === "undefined" || !('gtag' in window)) return undefined;
   
   return new Promise((resolve) => {
-    window.gtag?.('get', 'G-VLGZ0MSKT6', 'client_id', (clientId: string) => {
+    window.gtag?.('get', 'G-1LJBMEE5TP', 'client_id', (clientId: string) => {
       resolve(clientId);
     })
   });
